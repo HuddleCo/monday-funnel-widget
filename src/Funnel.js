393d551a6a79663aa652fe028ccc59dcd05f58c7
David Milanese <milanese.david@gmail.com>
2022-06-27 13:22:20 +0930

    chore: update funnel colors to alternate between 2 colors

--- src/Funnel.js
+++ src/Funnel.js
@@ -3,13 +3,13 @@ import { FunnelChart } from 'react-funnel-pipeline';
 import 'react-funnel-pipeline/dist/index.css';
 
 const Funnel = ({ data }) => {
-    return (
-        <FunnelChart
-            data={data}
-            style={{ width: '100%', height: '100%'}}
-            pallette = {['#3b7dd8', '#4a91f2', '#64a1f4', '#8dbdff', '#bfd6f6']}
-        />
-    );
+  return (
+    <FunnelChart
+      data={data}
+      style={{ width: '100%', height: '100%' }}
+      pallette={['#3b7dd8', '#64a1f4']}
+    />
+  );
 };
 
 export default Funnel;
