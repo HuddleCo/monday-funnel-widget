8f59dad819b08bd3a840c0a24e12ef5aca89f62e
David Milanese <milanese.david@gmail.com>
2022-07-13 16:55:19 +0930

    feat: display error when an error occours

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import mondaySdk from "monday-sdk-js";
 import "monday-ui-react-core/dist/main.css";
-import { Container } from "react-bootstrap";
+import { Alert, Container } from "react-bootstrap";
 import "react-funnel-pipeline/dist/index.css";
 
 import "./App.css";
@@ -42,29 +42,51 @@ class App extends React.Component {
     this.state = {
       store: {},
       settings: {},
+      error: null,
     };
   }
 
   componentDidMount() {
-    contextSettings().then(({ settings, context }) =>
-      getItemsPerGroupPerBoard(context.boardIds).then((store) => {
-        this.setState({ settings, store }, () => {
-          monday.listen("settings", ({ data: settings }) =>
-            this.setState({ settings })
-          );
-        });
-      })
-    );
+    contextSettings()
+      .then(({ settings, context }) =>
+        getItemsPerGroupPerBoard(context.boardIds).then((store) =>
+          this.setState({ settings, store, error: null }, () =>
+            monday.listen("settings", ({ data: settings }) =>
+              this.setState({ settings, error: null })
+            )
+          )
+        )
+      )
+      .catch((error) => this.state({ error }));
   }
 
   groupIds = () =>
     Object.values(this.state.settings.groupsPerBoard || {}).flat();
   funnelData = () => this.state.store || {};
+  displayError = (error = this.state.error) => {
+    if (error) {
+      return (
+        <div>
+          <Alert variant="danger">{error.message}</Alert>
+          <strong>this.state:</strong>
+          <pre>{JSON.stringify(this.state, null, 2)}</pre>
+        </div>
+      );
+    }
+  };
+
+  displayFunnel = () => {
+    try {
+      return <Funnel data={this.funnelData()} filters={this.groupIds()} />;
+    } catch (error) {
+      return this.displayError(error);
+    }
+  };
 
   render() {
     return (
       <Container className="mt-4">
-        <Funnel data={this.funnelData()} filters={this.groupIds()} />
+        {this.displayError() || this.displayFunnel()}
       </Container>
     );
   }
